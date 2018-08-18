a7df42d051a228e4fa1c8738d61b672d85063649
marcinj <marcinje@gmail.com>
2018-08-18 02:27:48 +0200

    Small fix

--- app/src/main/java/webview/marcin/biz/webviewtest/MainActivity.java
+++ app/src/main/java/webview/marcin/biz/webviewtest/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     if (savedInstanceState == null) {
       /*
+
       JSONObject obj = new JSONObject();
       try {
       } catch (JSONException e) {
